839e6353e6c8e9bbaf1d94b8f27b789d29325e3b
philipboboc1-debug <philipboboc1@gmail.com>
2026-03-28 00:12:29 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,6 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
+  bare: 'https://uv.student-info.xyz/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
